14efd30cf4aad18b558b0892f754a6d8dc76c2a3
Ryan Mills <ryan.mills@masabi.com>
2021-12-13 16:19:22 +0000

    More idomatic Kotlin and improvements on Day 13

--- src/main/kotlin/day13/Main.kt
+++ src/main/kotlin/day13/Main.kt
@@ -6,78 +6,83 @@ class Main {
     private fun getInputText(): String = Main::class.java.getResource("input.txt")?.readText()!!
 
     fun part1(): Int {
-        val coords = getInputText().split("\n\n")[0].split("\n").map { Pair(it.split(",")[0].toInt(), it.split(",")[1].toInt()) }.toSet()
-        val instructions = getInputText().split("\n\n")[1].split("\n")
+        val coords = getInputText()
+            .split("\n\n")[0]
+            .split("\n")
+            .map {
+                Pair(
+                    it.split(",")[0].toInt(),
+                    it.split(",")[1].toInt()
+                )
+            }.toSet()
 
-        // Just the first instructions
-        val dirs = instructions.map{it.subSequence(11, 12)}
-        val lines = instructions.map { it.subSequence(13, it.length).toString().toInt() }
+        val instructions = getInputText()
+            .split("\n\n")[1]
+            .split("\n")
+            .map {
+                Pair(
+                    it.subSequence(11, 12),
+                    (it.subSequence(13, it.length) as String).toInt()
+                )
+            }
 
-//        println(coords)
-
-       return (0..0).fold(coords) { acc, instrNo ->
-            val dir = dirs[instrNo]
-            val line = lines[instrNo]
-
-//           println("fold along $dir=$line")
-
-            val ret = acc.map {
+        return instructions.subList(0, 1).fold(coords) { acc, instr ->
+            val (dir, line) = instr
+            acc.map {
                 if (dir == "y" && it.second > line) {
-                    Pair(it.first, 2*line - it.second)
-                } else if(dir == "x" && it.first > line) {
-                    Pair(2*line - it.first, it.second)
+                    Pair(it.first, 2 * line - it.second)
+                } else if (dir == "x" && it.first > line) {
+                    Pair(2 * line - it.first, it.second)
                 } else {
                     it
                 }
             }.toSet()
-
-//           println(ret)
-
-           ret
         }.size
     }
 
     fun part2() {
-        val coords = getInputText().split("\n\n")[0].split("\n").map { Pair(it.split(",")[0].toInt(), it.split(",")[1].toInt()) }.toSet()
-        val instructions = getInputText().split("\n\n")[1].split("\n")
+        val coords = getInputText()
+            .split("\n\n")[0]
+            .split("\n")
+            .map {
+                Pair(
+                    it.split(",")[0].toInt(),
+                    it.split(",")[1].toInt()
+                )
+            }.toSet()
 
-        // Just the first instructions
-        val dirs = instructions.map{it.subSequence(11, 12)}
-        val lines = instructions.map { it.subSequence(13, it.length).toString().toInt() }
+        val instructions = getInputText()
+            .split("\n\n")[1]
+            .split("\n")
+            .map {
+                Pair(
+                    it.subSequence(11, 12),
+                    (it.subSequence(13, it.length) as String).toInt()
+                )
+            }
 
-//        println(coords)
+        val outGrid = mutableListOf<MutableList<Char>>()
 
-        val newCoords = (0..11).fold(coords) { acc, instrNo ->
-            val dir = dirs[instrNo]
-            val line = lines[instrNo]
-
-//            println("fold along $dir=$line")
-
-            val ret = acc.map {
+        instructions.fold(coords) { acc, instr ->
+            val (dir, line) = instr
+            acc.map {
                 if (dir == "y" && it.second > line) {
-                    Pair(it.first, 2*line - it.second)
-                } else if(dir == "x" && it.first > line) {
-                    Pair(2*line - it.first, it.second)
+                    Pair(it.first, 2 * line - it.second)
+                } else if (dir == "x" && it.first > line) {
+                    Pair(2 * line - it.first, it.second)
                 } else {
                     it
                 }
             }.toSet()
-
-//            println(ret)
-
-            ret
-        }
-
-        val outGrid = mutableListOf<MutableList<Char>>()
-
-        for(coord in newCoords) {
-            while(outGrid.size <= coord.second) {
-                outGrid.add(mutableListOf())
-            }
-            while (outGrid[coord.second].size <= coord.first) {
-                outGrid[coord.second].add(' ')
-            }
-            outGrid[coord.second][coord.first] = '#'
+        }.forEach { coord ->
+            val (x, y) = coord
+            outGrid.addAll(
+                (outGrid.size..y).map { MutableList(x) { ' ' } }
+            )
+            outGrid[y].addAll(
+                (outGrid[y].size..x).map { ' ' }
+            )
+            outGrid[y][x] = '#'
         }
 
         outGrid.forEach { rows ->
