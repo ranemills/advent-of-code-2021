0ebc1564f775a884174c2b4de18d63aa604f0f69
Ryan Mills <ranemills@googlemail.com>
2022-01-01 19:36:51 +0000

    Some updates

--- src/main/kotlin/com/mills/advent/twentyone/Day23Djikstra.kt
+++ src/main/kotlin/com/mills/advent/twentyone/Day23Djikstra.kt
@@ -49,9 +49,9 @@ class Day23Djikstra : AdventOfCode {
             .associate { it }
 
         var currentGrid: Map<Coord, Char> = grid
-//        val gridCosts = mutableMapOf(currentGrid to 0)
+        val gridCosts = mutableMapOf(currentGrid to 0)
         val gridCostsBackup = mutableMapOf(currentGrid to 0)
-        val gridCosts = sortedMapOf(0 to mutableSetOf(currentGrid))
+//        val gridCosts = sortedMapOf(0 to mutableSetOf(currentGrid))
         val visitedNodes = mutableSetOf(grid)
         val previous: MutableMap<Map<Coord,Char>, Map<Coord,Char>> = mutableMapOf()
 
@@ -72,6 +72,10 @@ class Day23Djikstra : AdventOfCode {
                 newGrid[end] = startValue
                 newGrid[start] = '.'
 
+                if(newGrid == currentGrid) {
+                    throw IllegalStateException()
+                }
+
                 newGrid to gridCostsBackup[currentGrid]!! + cost
             }.filter {
                 it.key !in visitedNodes
@@ -79,26 +83,24 @@ class Day23Djikstra : AdventOfCode {
                 val storedCost = gridCostsBackup.getOrDefault(neighbour, MAX)
                 if(storedCost > cost) {
                     gridCostsBackup[neighbour] = cost
-                    previous[neighbour] = currentGrid
-
-                    val storedSet = gridCosts.computeIfAbsent(storedCost) { mutableSetOf() }
-                    storedSet.remove(neighbour)
-                    if(storedSet.isEmpty()) {
-                        gridCosts.remove(storedCost)
-                    } else {
-                        gridCosts[storedCost] = storedSet
-                    }
-
-                    val newSet = gridCosts.computeIfAbsent(cost) { mutableSetOf() }
-                    newSet.add(neighbour)
-                    gridCosts[cost] = newSet
+                    gridCosts[neighbour] = cost
                 }
             }
 
             visitedNodes.add(currentGrid)
+            gridCosts.remove(currentGrid)
 
-            val nextGrid = gridCosts[gridCosts.firstKey()]?.first()
-            currentGrid = nextGrid!!
+            val nextGrid = gridCosts.minByOrNull { it.value }?.key
+
+            if(currentGrid == nextGrid) {
+                throw IllegalStateException()
+            }
+
+            if(nextGrid == null) {
+                throw IllegalStateException()
+            }
+
+            currentGrid = nextGrid
         }
 
         var printingGrid = currentGrid
@@ -111,6 +113,16 @@ class Day23Djikstra : AdventOfCode {
         return gridCostsBackup[currentGrid]!!
     }
 
+    fun removeGridCost(gridCosts: SortedMap<Int, MutableSet<Map<Coord, Char>>>, grid: Map<Coord, Char>, cost: Int) {
+        val storedSet = gridCosts.computeIfAbsent(cost) { mutableSetOf() }
+        storedSet.remove(grid)
+        if(storedSet.isEmpty()) {
+            gridCosts.remove(cost)
+        } else {
+            gridCosts[cost] = storedSet
+        }
+    }
+
     fun Map<Coord, Char>.print() {
         (0..this.maxOf { it.key.y }).forEach { y ->
             (0..this.maxOf {it.key.x}).forEach { x ->
@@ -121,18 +133,32 @@ class Day23Djikstra : AdventOfCode {
         println()
     }
 
+    val stepCountCache = mutableMapOf<Pair<Coord, Coord>, List<Coord>>()
+
     fun getValidMoves(grid: Map<Coord, Char>): List<Pair<Coord, Coord>> {
         val emptyCorridorSpaces = corridorCoords.filter {
             grid[it] == '.'
+        }
+
+        val lastEmptyForRoom =  mapOf('A' to aRoom, 'B' to bRoom, 'C' to cRoom, 'D' to dRoom).mapValues { (char, room) ->
+            val room0Value = grid[room[0]]
+            val room1Value = grid[room[1]]
+
+            when {
+                room0Value != '.' -> null
+                room1Value == '.' -> room[1]
+                room1Value == char -> room[0]
+                else -> null
+            }
         }
 
         val outOfRoomMoves = mapOf('A' to aRoom, 'B' to bRoom, 'C' to cRoom, 'D' to dRoom).flatMap { (char, room) ->
             val room0Value = grid[room[0]]
             val room1Value = grid[room[1]]
             when {
-                room1Value != char && room0Value == '.' -> listOf(room1Value!! to room[1])
-                room1Value != char && room0Value == char -> listOf(room0Value to room[0])
-                room0Value != char -> listOf(room0Value!! to room[0])
+                room0Value == char && room1Value == char -> listOf()
+                room0Value != '.' -> listOf(room0Value!! to room[0])
+                room1Value !in listOf('.', char) -> listOf(room1Value to room[1])
                 else -> listOf()
             }
         }.flatMap { (char, roomCoord) ->
@@ -140,21 +166,7 @@ class Day23Djikstra : AdventOfCode {
                 roomCoord to corridorCoord
             }
 
-            val otherRoom = getRoom(char)
-
-            val otherRoomPair = if(otherRoom.isNotEmpty()) {
-                val room0Value = grid[otherRoom[0]]
-                val room1Value = grid[otherRoom[1]]
-
-                when {
-                    room0Value == '.' && room1Value == '.' -> roomCoord to otherRoom[1]
-                    room0Value == '.' && room1Value == char -> roomCoord to otherRoom[0]
-                    else -> null
-                }
-            } else null
-
-            if(otherRoomPair != null) corridorPairs + otherRoomPair
-            else corridorPairs
+            lastEmptyForRoom[char]?.let { corridorPairs + (roomCoord to it) } ?: corridorPairs
         }
 
         val outOfCorridorMoves = corridorCoords.associateWith { corridorCoord ->
@@ -162,45 +174,30 @@ class Day23Djikstra : AdventOfCode {
         }.filter {
             it.value != '.'
         }.flatMap { (corridorCoord, char) ->
-            val room = getRoom(char)
-            if (grid[room[0]] == '.') {
-                when (grid[room[1]]) {
-                    '.' -> listOf(corridorCoord to room[1], corridorCoord to room[0])
-                    char -> listOf(corridorCoord to room[0])
-                    else -> listOf()
-                }
-            } else {
-                listOf()
-            }
+            lastEmptyForRoom[char]?.let { listOf(corridorCoord to it) } ?: listOf()
         }
 
         return (outOfRoomMoves + outOfCorridorMoves).filter { (from, to) ->
-            // is it a clear path?
-
-            val steps = if (from.y == 1) {
-                // from corridor to room
-//                Coord(to.x, from.y)
-                (min(from.x, to.x)..max(from.x, to.x)).map { x -> Coord(x, from.y) } +
-                        (min(from.y, to.y)..max(from.y, to.y)).map { y -> Coord(to.x, y) }
-            }
-            else if(to.y == 1) {
-                // from room to corridor
-//                Coord(from.x, to.y)
-
-                (min(from.x, to.x)..max(from.x, to.x)).map { x -> Coord(x, to.y) } +
-                        (min(from.y, to.y)..max(from.y, to.y)).map { y -> Coord(from.x, y) }
-            } else {
-                // from room to room
-                (min(from.x, to.x)..max(from.x, to.x)).map { x -> Coord(x, 1) } +
-                        (1..from.y).map { y -> Coord(from.x, y) } +
-                        (1..to.y).map { y -> Coord(to.x, y) }
+            val steps = stepCountCache.computeIfAbsent(Pair(from, to)) {
+                if (from.y == 1) {
+                    // from corridor to room
+                    (min(from.x, to.x)..max(from.x, to.x)).map { x -> Coord(x, from.y) } +
+                            (min(from.y, to.y)..max(from.y, to.y)).map { y -> Coord(to.x, y) }
+                } else if (to.y == 1) {
+                    // from room to corridor
+                    (min(from.x, to.x)..max(from.x, to.x)).map { x -> Coord(x, to.y) } +
+                            (min(from.y, to.y)..max(from.y, to.y)).map { y -> Coord(from.x, y) }
+                } else {
+                    // from room to room
+                    (min(from.x, to.x)..max(from.x, to.x)).map { x -> Coord(x, 1) } +
+                            (1..from.y).map { y -> Coord(from.x, y) } +
+                            (1..to.y).map { y -> Coord(to.x, y) }
+                }.filter {
+                    it != from
+                }
             }
 
-
-
-            steps.filter {
-                it != from
-            }.all {
+            steps.all {
                 grid[it] == '.'
             }
 
